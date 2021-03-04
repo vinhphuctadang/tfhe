f1dbdf303f7870331763b88cf7edf9a2a0513434
Vinh Phuc Ta Dang <vinhphuctadang@gmail.com>
2021-03-04 08:37:52 +0700

    fix script for darwin

--- run.sh
+++ run.sh
@@ -1,5 +1,10 @@
+if [[ $OSTYPE == "darwin"*  ]]; then 
+    EXT="dylib"
+else
+    EXT="so"
+fi
 # run the script including the tfhe lib
-if g++ ${1} /usr/local/lib/*.so -o tmp; then
+if g++ ${1} /usr/local/lib/*.${EXT} -o tmp; then
     ./tmp
     rm tmp
 fi
